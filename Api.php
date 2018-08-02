5e099cb67f2b8656572d7f60f3fb33f71d7392ca
Golap Hazi <golaphazi@gmail.com>
2018-08-02 12:44:28 +0600

    Update Api.php

--- Api.php
+++ Api.php
@@ -28,7 +28,8 @@ class Api {
 	*
 	====================================================*/
 	public function headers(){
-        header('Cache-Control: no-cache, must-revalidate');
+        header_remove();
+	header('Cache-Control: no-cache, must-revalidate');
         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // JSONs are by default dynamic data
         header('Content-type: application/json');
     }
